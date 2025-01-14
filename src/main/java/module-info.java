1e79cb8452da40ac1ce1aaca76a5f93c7128463e
Marc Beukenboom <505086@student.fontys.nl>
2025-01-14 10:21:13 +0100

    BCrypt
    Alles snel gefixed, gebruikt nu BCrypt maar is niet helemaal safe voor SQL injections. Moet nieuwe informatie in database zetten om het werkend te krijgen. Dit is specifiek BCrypt, ga nu beginnen aan switch screens.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.guardiansofthewasteland.orginiele_gui {
     requires javafx.fxml;
     requires java.sql;
     requires javax.servlet.api;
+    requires jbcrypt;
 
 
     opens org.guardiansofthewasteland.orginiele_gui to javafx.fxml;
--- src/main/java/org/guardiansofthewasteland/orginiele_gui/LoginscreenController.java
+++ src/main/java/org/guardiansofthewasteland/orginiele_gui/LoginscreenController.java
@@ -1,6 +1,7 @@
 package org.guardiansofthewasteland.orginiele_gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
 import javafx.fxml.FXMLLoader;
@@ -8,8 +9,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
-
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Timer;
+import java.util.TimerTask;
+import org.mindrot.jbcrypt.BCrypt;
+
+
+
 
 public class LoginscreenController {
     @FXML
@@ -34,20 +44,16 @@ public class LoginscreenController {
         this.previousStage = stage;
     }
 
-
     @FXML
     private void openNewScene() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("HomeScreen.fxml"));
             Parent root = loader.load();
 
-
             HomescreenController homeController = loader.getController();
             homeController.setCurrentStage(currentstage);
 
-
             currentstage.close();
-
 
             Stage newStage = new Stage();
             newStage.setScene(new Scene(root));
@@ -55,29 +61,67 @@ public class LoginscreenController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
-
-
     }
+
     @FXML
-    private void LoginConfirmation () {
+    private void CheckLogin() {
         String username = UsernameBox.getText();
         String password = PasswordBox.getText();
 
-        if (username.equals("admin") && password.equals("admin")) {
-            System.out.println("Login Successful");
+        if (LoginConfirmation(username, password)) {
             openNewScene();
-
         } else {
-            System.out.println("Login Failed");
+            System.out.println("Login failed. Please check your username and password.");
         }
     }
 
+    @FXML
+    private boolean LoginConfirmation(String username, String password) {
+        Connection connection = null;
+        Statement preparedStatement = null;
+        ResultSet resultSet = null;
 
+        try {
+            connection = DatabaseConnect.getConnection();
+
+            if (connection != null) {
+                System.out.println("Connection established!");
+
+
+                String query = "SELECT password FROM medewerker WHERE username = '" + username + "'";
+                preparedStatement = connection.prepareStatement(query);
+
+
+                resultSet = preparedStatement.executeQuery(query);
+
+                if (resultSet.next()) {
+                    String storedHashedPassword = resultSet.getString("password");
+
+
+                    if (BCrypt.checkpw(password, storedHashedPassword)) {
+                        return true;
+                    }
+                }
+            } else {
+                System.out.println("Failed to connect to the database.");
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL error occurred");
+            e.printStackTrace();
+        } finally {
+
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return false;
+    }
 }
-
 
 
 
